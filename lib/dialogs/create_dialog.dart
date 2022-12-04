6988a7a9740c1a7eaae24c8e425691c90b24a8c7
Eric <eric@bostrot.com>
2022-12-04 22:09:33 +0100

    Fixed tag string

--- lib/dialogs/create_dialog.dart
+++ lib/dialogs/create_dialog.dart
@@ -92,8 +92,12 @@ Future<void> createInstance(
       // Remove prefix
       distroName = autoSuggestBox.text.split('dockerhub:')[1];
       // Get tag
-      String? image = distroName.split(':')[0];
-      String? tag = distroName.split(':')[1];
+      List<String> split = distroName.split(':');
+      String? image = split[0];
+      String? tag;
+      if (split.length > 1) {
+        tag = split[1];
+      }
 
       bool isDownloaded = false;
       // Check if image already downloaded
@@ -114,7 +118,7 @@ Future<void> createInstance(
           String percentage =
               (currentStep / totalStep * 100).toStringAsFixed(0);
           Notify.message('${'downloading-text'.i18n()}'
-              ' Layer ${current + 1}/$total: $percentage% ($progressInMB MB/$totalInMB MB)');
+              ' Layer ${current + 1}/$total: $percentage% ($progressInMB MB)');
         });
         Notify.message('downloaded-text'.i18n());
         // Set distropath with distroName
